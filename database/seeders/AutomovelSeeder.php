faca3956d7463655084e3162c46046a9c1979fea
Gabriel <gts.senna@gmail.com>
2021-06-27 21:22:46 -0300

    update with new column

--- database/seeders/AutomovelSeeder.php
+++ database/seeders/AutomovelSeeder.php
@@ -15,21 +15,21 @@ class AutomovelSeeder extends Seeder
     {
         ModelAutomovel::create([ 
             'id_user' => 1,
-            'tipo' => 'Carro',
+            'id_tipo' => 1,
             'marca' => 'Toyota',
             'modelo' => 'Corolla',
             'versao' => '2021'
         ]);        
         ModelAutomovel::create([ 
             'id_user' => 2,
-            'tipo' => 'Moto',
+            'id_tipo' => 2,
             'marca' => 'Honda',
             'modelo' => 'Bis',
             'versao' => '2014'
         ]);  
         ModelAutomovel::create([ 
             'id_user' => 1,
-            'tipo' => 'Caminhão',
+            'id_tipo' => 3,
             'marca' => 'Scania',
             'modelo' => 'L51', 
             // 'versao' => null,
